268ae0f462b3b445de6c900ecce96fb197d4e892
Sanam_App <ugyendechen00@gmail.com>
2023-12-11 13:00:29 +0600

    Node

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const axios = require('axios');
+
+const app = express();
+
+const dbUrl = "mongodb+srv://nomindbhutan:ZJEd2PhLifmzvUDQ@fbscraper.taussv1.mongodb.net/?retryWrites=true&w=majority"
+
+// Connect to the database
+mongoose.connect(dbUrl, {
+    // Add necessary options if required
+})
+    .then(() => {
+        console.log('Connected to database');
+    })
+    .catch((err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    });
+
+// Define the Post model only once
+const Post = mongoose.model('Post', {
+    message: String,
+    created_time: Date
+});
+
+const fetchAndStoreData = async () => {
+    try {
+        const accessToken = "EAAKDwGItCwYBO916J7F8jUB3kAslRZCyr3hgYULIKKktuoV7pbf9MiZCA2UubRwLJ4mqjMhQZCTgKDPh4YoA4RQFJVStgOHrFKNNigbiZAxrWfrl4tP3Wz87Ho7TrZAAMF1spT7ZCaoNnzFAzVRjGDRklDeMnBZAI45x5FMeoZCxG0sD2fOOT2CuWucfRFjrzEohKbnYyjSx1tZBMTo5q2c5YUocZD";
+        const pageId = 101550476333422;
+        const apiUrl = `https://graph.facebook.com/v18.0/${pageId}/?fields=posts{message,created_time}&access_token=${accessToken}`;
+
+        const response = await axios.get(apiUrl);
+
+        if (response.data && response.data.posts && response.data.posts.data) {
+            const fetchedPosts = response.data.posts.data;
+
+            for (const fetchedPost of fetchedPosts) {
+                // Check if the post already exists in the database by its unique identifier (e.g., message or created_time)
+                const existingPost = await Post.findOne({
+                    message: fetchedPost.message,
+                    created_time: fetchedPost.created_time
+                });
+
+                // If the post doesn't exist, insert it into the database
+                if (!existingPost) {
+                    await Post.create({
+                        message: fetchedPost.message,
+                        created_time: fetchedPost.created_time
+                    });
+                    console.log('New post added:', fetchedPost);
+                }
+            }
+            console.log('Data fetched and stored successfully');
+        } else {
+            console.error('API response format incorrect or missing data');
+        }
+    } catch (error) {
+        console.error('Error fetching and storing data:', error);
+    }
+};
+
+// Fetch and store data initially when the server starts
+fetchAndStoreData();
+
+// Set up an interval to fetch and store data every 10 minutes (600,000 milliseconds)
+setInterval(fetchAndStoreData, 50000);
+
+app.get('/posts', async (req, res) => {
+    try {
+        // Fetch data from the database
+        const posts = await Post.find({}).exec();
+        res.json(posts); // Send the fetched data as a JSON response
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Example listening port, replace 3030 with your desired port number
+app.listen(3030, () => {
+    console.log('Server is running on port 3030');
+});
